3ebd74e3bdf3f4148d91b95c81c3ac186c90d963
SidV <SidVal@users.noreply.github.com>
2018-01-12 14:30:16 -0300

    Create v0.cs

---
+++ csharp/codigos/condiciones-pagos-di-api/v0.cs
@@ -0,0 +1,23 @@
+/*
+Fuente: http://foros.consultoria-sap.com/t/llenar-una-grilla-con-las-formas-de-pago-con-diapi/20158
+Autor/es: GrabrielGS 
+*/
+
+Dim oRecordSet As SAPbobsCOM.Recordset
+  Dim sCodigoCondicion as Int
+  Dim sNombreCondicion as String
+
+  oRecordSet = oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+
+  oRecordSet.DoQuery("SELECT * FROM OCTG")
+  
+  oRecordSet.MoveFirst()
+
+  While Not oRecordSet.EoF
+
+    sCodigoCondicion  = oRecordSet .Fields.Item("GroupNum").Value
+    sNombreCondicion  = oRecordSet .Fields.Item("PymntGroup").Value)
+
+    oRecordSet .MoveNext()
+
+  End While
